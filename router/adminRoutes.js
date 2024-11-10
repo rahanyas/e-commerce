b9ec3a1e816e82f5bda17225af2a7f5c277c03ff
rahanyas <rahanyas3@gmail.com>
2024-11-10 15:50:05 +0530

    making stripe procced btn toggle

--- router/adminRoutes.js
+++ router/adminRoutes.js
@@ -1,7 +1,20 @@
 import express from "express";
 const adminrouter = express.Router();
-import {adminLoginPage, adminLoginAuth, manageUser, editUserPage, editUser, addUserPage, addUserAuth, adminHomePage} from "../controllers/adminController/userController.js";
-import {UserPagination} from "../public/helpers/pagination.js";
+import { adminLoginPage, 
+         adminLoginAuth, 
+         manageUser, 
+         editUserPage, 
+         editUser, 
+         addUserPage, 
+         addUserAuth, 
+         adminHomePage
+        } 
+from "../controllers/adminController/userController.js";
+
+import {
+        UserPagination
+       } 
+from "../public/helpers/pagination.js";
 
 adminrouter.get('/adminLogin', adminLoginPage);
 adminrouter.post('/adminLoginAuth', adminLoginAuth);
