9394abfc3e80f240a701a89e6dea17425e05bdbb
RAHANYAS <153750452+rahanyas@users.noreply.github.com>
2025-04-27 22:31:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
- import express from 'express';           
+  import express from 'express';           
 import dotenv from 'dotenv';              
 import connectDB from './config/db.js';    
 import session from 'express-session'; 
